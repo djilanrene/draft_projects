dcb5c58d3ab7674784a8fa348e5a7396673db826
René DJILAN <djilanrenejunior@gmail.com>
2025-11-20 13:16:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-274979289-18df3",
+  "appId": "1:557949791242:web:a194ace127207a43bc83f3",
+  "apiKey": "AIzaSyBHe2hBvEPPj5tSpfIK8dgXSZEk6KycAaA",
+  "authDomain": "studio-274979289-18df3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "557949791242"
+};
